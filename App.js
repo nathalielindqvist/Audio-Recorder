ec86b7b5317083a31ac0ea3a10cd9456e4f27124
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-05-31 18:58:11 +0200

    Navigation

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import HomeScreen from "./screens/Home";
+import ListScreen from "./screens/List";
+import AudioScreen from "./screens/Audio";
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Start" }}
+        />
+        <Stack.Screen
+          name="List"
+          component={ListScreen}
+          options={{ title: "List" }}
+        />
+        <Stack.Screen
+          name="Audio"
+          component={AudioScreen}
+          options={{ title: "Audio" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
