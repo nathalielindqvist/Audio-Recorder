1850f794659789d844724562e35b16a03d32cf0c
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-05-31 18:58:58 +0200

    List screen

---
+++ screens/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Button, Text, Stylesheet } from "react-native";
+import AudioRecorder from "./Audio";
+
+export default function List() {
+  console.log(recordings);
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
