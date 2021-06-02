4fe507e9509dc9eef32b2bbfc3e2dec09da77f95
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-06-02 12:45:26 +0200

    Trail flatlist

--- screens/List.js
+++ screens/List.js
@@ -1,26 +1,56 @@
 import React, { Component } from "react";
-import { View, Button, Text, Stylesheet } from "react-native";
+import { View, Button, Text, Stylesheet, FlatList } from "react-native";
 import store from "./store";
+import { connect } from "react-redux";
 
-export default class List extends Component {
+// store.subscribe(() => {
+//   const state = store.getState().recordingsArray;
+//   console.log(state);
+// });
+
+const ItemView = () => {
+  return (
+    <ListItem bottomDivider>
+      <Text>{title}</Text>
+    </ListItem>
+  );
+};
+
+class List extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      recordingsArray: [],
-    };
+    // this.state = {
+    //   recordingsArray: [],
+    // };
 
-    store.subscribe(() => {
-      this.setState({
-        recordingsArray: store.getState().recordingsArray,
-      });
-    });
+    // store.subscribe(() => {
+    //   this.setState({
+    //     recordingsArray: store.getState().recordingsArray,
+    //   });
+    //   console.log(this.state.recordingsArray);
+    // });
   }
+
   render() {
     return (
       <View>
-        <Text>{this.state.recordingsArray}</Text>
+        <Text>Hej</Text>
+        <FlatList>
+          data={this.props.recordingsArray}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={ItemView}
+        </FlatList>
       </View>
     );
   }
 }
+
+function mapStateToProps(state) {
+  console.log(state.recordingsArray);
+  return {
+    recordingsArray: state.recordingsArray,
+  };
+}
+
+export default connect(mapStateToProps)(List);
